29d54079a95fc4c5ad590ea1ef88b76b638ffb54
anonymousmovies2023 <130214949+anonymousmovies2023@users.noreply.github.com>
2023-07-04 09:30:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
+  git clone https://github.com/anonymousmovies2023/permiumbot.git /premiumBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /permiumBot
 fi
-cd /DQTheFileDonorBot
+cd /premiumBot
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting permiumbot...."
 python3 bot.py
